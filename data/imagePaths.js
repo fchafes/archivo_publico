0dac72eb0f5e6cb322a05eadd0a5439d2a2632a2
fchafes <64428168+fchafes@users.noreply.github.com>
2024-12-07 19:54:58 -0300

    update

---
+++ data/imagePaths.js
@@ -0,0 +1,11 @@
+export const imagePaths = [
+    "/images/home/home_1.jpg",
+    "/images/home/home_2.jpg",
+    "/images/home/home_3.jpg",
+    "/images/home/home_4.jpg",
+    "/images/home/home_5.jpg",
+    "/images/home/home_6.jpg",
+    "/images/home/home_7.jpg",
+    "/images/home/home_8.jpg",
+    "/images/home/home_9.jpg",
+  ];
