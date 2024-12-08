8a448269dfd7a0b0cc62c25f9568d0d0cb850465
fchafes <64428168+fchafes@users.noreply.github.com>
2024-12-08 18:22:52 -0300

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,16 @@
 import { imagePaths } from "../../data/imagePaths";
 import styles from "./page.module.css";
+import Image from "next/image";
 // import { gsap } from "gsap";
 // import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 export default function Home() {
-
   return (
     <div className={styles.imageSlider}>
       {imagePaths.map((src, index) => (
         <div key={index} className={styles.imageBox}>
           <img className={styles.img} src={src} alt={`Imagen ${index + 1}`} />
+          <Image src={src} width={500} height={500} alt="imagen-next"/>
         </div>
       ))}
     </div>
