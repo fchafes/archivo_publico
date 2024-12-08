592fa6ee922106b0a3ac3529147d5d0c628a54a7
fchafes <64428168+fchafes@users.noreply.github.com>
2024-12-08 19:10:15 -0300

    update slider finalmente

--- src/app/page.js
+++ src/app/page.js
@@ -8,24 +8,9 @@ export default function Home() {
   console.log("esto es imagePaths", imagePaths);
   return (
     <div className={styles.imageSlider}>
-      <Image
-        src="/images/logotipo_ap.png"
-        alt="Logotipo de Archivo Público"
-        layout="responsive"
-        width={400}
-        height={56}
-        priority
-      />
-      <Image
-        src="/images/slider/home_10.jpg"
-        width={500}
-        height={500}
-        alt="imagen-next"
-      />
       {imagePaths.map((src, index) => (
         <div key={index} className={styles.imageBox}>
           <img className={styles.img} src={src} alt={`Imagen ${index + 1}`} />
-          {/* <Image src={src} width={500} height={500} alt="imagen-next" /> */}
         </div>
       ))}
     </div>
