f2d119cd26c5e7c5cd61cb7f456795d18490539a
fchafes <64428168+fchafes@users.noreply.github.com>
2025-01-20 10:49:49 -0300

    hago horizontal scroll en home

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,25 @@
+"use client";
+
+import { useEffect } from "react";
 import { imagePaths } from "../../data/imagePaths";
 import styles from "./page.module.css";
-// import { gsap } from "gsap";
-// import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 export default function Home() {
+  useEffect(() => {
+    const scrollContainer = document.querySelector(`.${styles.imageSlider}`);
+
+    const onWheel = (event) => {
+      event.preventDefault();
+      scrollContainer.scrollLeft += event.deltaY;
+    };
+
+    scrollContainer.addEventListener("wheel", onWheel);
+
+    return () => {
+      scrollContainer.removeEventListener("wheel", onWheel);
+    };
+  }, []);
+
   return (
     <div className={styles.imageSlider}>
       {imagePaths.map((src, index) => (
